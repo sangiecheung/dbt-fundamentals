c314ef141bbf117172b8641beee5355ab078c6c9
sangie.cheung@baruchmail.cuny.edu <sangie.cheung@baruchmail.cuny.edu>
2022-04-23 07:29:22 +0000

    Generic and singular tests

--- models/staging/jaffle_shop/stg_orders.sql
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -1,10 +1,6 @@
-with orders as (
- select
+select
     id as order_id,
     user_id as customer_id,
     order_date,
     status
- from `dbt-tutorial.jaffle_shop.orders`
-)
-
-select * from orders
+from {{ source('jaffle_shop', 'orders') }}
